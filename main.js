dc3d712e736f86cc8ee2ab00cc983732f5d54689
Salvatore Rinchiera <salvatore@rinchiera.com>
2014-05-31 15:17:02 -0400

    Added caching support and rate limit support
    Will first query a redis database to retreive the time. If that fails
    then it queries the Google API directly.
    
    If we reach our rate limit (currently seems like 2 per second) then we
    wait a second before trying to request again.

--- main.js
+++ main.js
@@ -1,18 +1,58 @@
-var details_list = $('.details h5');
+function display_times() {
+  /* Displays travel time next to all listings */
 
-for (var i = 0; i < details_list.length; i++) {
-  fill_time(details_list[i]);
+  var details_list = $('.details h5');
+
+  for (var i = 0; i < details_list.length; i++) {
+    display_time(details_list[i]);
+  }
 }
 
 function format_origin(origin) {
-// Specifies New York, New York in address
+  /* Specifies New York, New York in address */
   return origin + ', New York, New York';
 }
 
-function fill_time(elm) {
+function origin_from_elm(elm) {
+  /* Given DOM element of listing details, return origin */
 
-  var origin = format_origin($(elm).find('a').text())
-  console.log(origin);
+  var origin = $(elm).find('a').text();
+  return format_origin(origin);
+}
+
+function append_time_to_elm(elm, time) {
+  /* Appends time string to the element */
+
+  $(elm).append('&nbsp;&nbsp;' + time);
+}
+
+function google_api_call(elm, data) {
+  /* Makes the actual API call and adds time to DOM on success */
+
+  var wait_milliseconds = 1000;
+
+  $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/directions/json',
+    data: data,
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log("Error retrieving data from Google Maps API!");
+      console.log(errorThrown);
+    },
+    success: function(response, textStatus, jgXHR) {
+      if (response['status'] == "OVER_QUERY_LIMIT") {
+        setTimeout(function() { google_api_call(elm, data) }, wait_milliseconds);
+      } else {
+        var time = response['routes'][0]['legs'][0]['duration']['text'];
+        append_time_to_elm(elm, time);
+      }
+    },
+  });
+}
+
+function google_api_display(elm) {
+  /* Dispalys result from Google Maps API call to DOM */
+
+  var origin = origin_from_elm(elm);
 
   var data = {
     'key': config['api_key'],
@@ -23,17 +63,33 @@ function fill_time(elm) {
     'arrival_time': '1400749479',
   }
 
+  google_api_call(elm, data);
+}
+
+function redis_display(elm) {
+  /* Displays result from redis server to DOM */
+
+  var origin = origin_from_elm(elm);
+  encoded_origin = encodeURIComponent(origin)
   $.ajax({
-    url: 'https://maps.googleapis.com/maps/api/directions/json',
-    data: data,
+    url: 'http://localhost/' + encoded_origin,
     error: function(jqXHR, textStatus, errorThrown) {
-      console.log("Google Maps API error!");
+      console.log("Error retrieving data from Redis server!");
       console.log(errorThrown);
     },
+    dataType: 'json',
     success: function(data, textStatus, jgXHR) {
-      console.log(data);
-      var time = data['routes'][0]['legs'][0]['duration'];
-      $(elm).append('&nbsp;&nbsp;' + time['text']);
-    },
+      if (data['status'] == 'success') {
+        append_time_to_elm(elm, data['time']);
+      } else {
+        google_api_display(elm);
+      }
+    }
   });
 }
+
+function display_time(elm) {
+  redis_display(elm);
+}
+
+display_times();
