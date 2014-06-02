02e99f10835092312071107ada554b25ba1f67f0
Chris Rinchiera <crinchie@villanova.edu>
2014-06-01 22:31:52 -0400

    Delete background

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  /* Drop-down menu to adjust settings including:
-   * Commute type: Driving, Bicycle, Transit, Walking
-   * Destination: Where are you commuting to?
-   * Cache-server: Where do you want to go before you contact Google API directly?
-   */
-});
